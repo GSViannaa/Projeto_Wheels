3c3b1900ba7950b9fda5a49dd21570d6eefffcab
AT Java <bernardo.baims@gmail.com>
2025-05-12 09:26:41 -0300

    criacao do bot

---
+++ src/main/java/com/ProjetoWheels/chat/Biketron3000.java
@@ -0,0 +1,16 @@
+package com.ProjetoWheels.chat;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+public class Biketron3000 extends TelegramLongPollingBot {
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    @Override
+    public String getBotUsername() {
+        return "";
+    }
+}
